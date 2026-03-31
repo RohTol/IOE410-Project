271f9b9e31b9232cb8a013eb1eef3354f5ed61dd
rohtol <rohtol@umich.edu>
2026-03-31 19:18:27 -0400

    code done

--- fulfillment.py
+++ fulfillment.py
@@ -35,8 +35,95 @@ for i in nodes:
     for j in nodes:
         dist[i, j] = euclidean(i, j)
 
-print(f"n={n}, k1={k1}, k2={k2}, C1={C1}, C2={C2}, L={L}")
-print(f"demand: {demand}")
-print(f"dist[0,5]={dist[0,5]:.3f}")
+
+
+# Now, we build the actual model
+it1 = tm.time()
+IPmod = Model("fulfillment centers")
+
+# We first define all of our decision variables
+x = IPmod.addVars(nodes, vtype=GRB.BINARY, name="x")
+y = IPmod.addVars(nodes, vtype=GRB.BINARY, name="y")
+z = IPmod.addVars(nodes, nodes, vtype=GRB.BINARY, name="z")
+w = IPmod.addVars(nodes, nodes, vtype=GRB.BINARY, name="w")
+q = IPmod.addVars(nodes, nodes, nodes, vtype=GRB.BINARY, name="q")
+
+# Next, we set out objective function
+IPmod.setObjective(
+    quicksum(L * dist[u,v] * z[u,v] for u in nodes for v in nodes) +
+    quicksum(demand[i] * dist[v, i] * w[v, i] for v in nodes for i in nodes),
+    GRB.MINIMIZE
+)
+
+# Next, we list our constraints:
+# 1. Each local FC v must be connected to exactly 1 regional FC if open, and 0 if closed
+for v in nodes:
+    IPmod.addConstr(quicksum(z[u, v] for u in nodes) == x[v])
+
+# 2. Each demand location i must be connected to exactly 1 local FC
+for i in nodes:
+    IPmod.addConstr(quicksum(w[v, i] for v in nodes) == 1)
+
+# 3. A demand location i can only be assigned to a local FC v if v is open
+for v in nodes:
+    for i in nodes:
+        IPmod.addConstr(w[v, i] <= x[v])
+
+# 4. A local FC v can only be assigned to a regional FC u if u is open
+for u in nodes:
+    for v in nodes:
+        IPmod.addConstr(z[u, v] <= y[u])
+
+# 5. A maximum of k1 local FCs can be opened
+IPmod.addConstr(quicksum(x[v] for v in nodes) <= k1)
+
+# 6. A maximum of k2 regional FCs can be opened
+IPmod.addConstr(quicksum(y[u] for u in nodes) <= k2)
+
+# 7. The sum of the items processed by local FC v is less than C1
+for v in nodes:
+    IPmod.addConstr(quicksum(demand[i] * w[v, i] for i in nodes) <= C1)
+
+# 8. The sum of the items processed by regional FC u is less than C2
+for u in nodes:
+    IPmod.addConstr(quicksum(demand[i] * q[u, v, i] for i in nodes for v in nodes) <= C2)
+
+# 8a, 8b: If at least 1 of w(v, i) or z(u, v) is 0, then q(u, v, i) = 0
+# 8c: If both w(v, i) = 1 and z(u, v) = 1, then q(u, v, i) = 1
+for u in nodes:
+    for v in nodes:
+        for i in nodes:
+            IPmod.addConstr(q[u, v, i] <= w[v, i]) #8a
+            IPmod.addConstr(q[u, v, i] <= z[u, v]) #8b
+            IPmod.addConstr(q[u, v, i] >= w[v, i] + z[u, v] - 1) #8c
+
+IPmod.optimize()
+it2 = tm.time()
+
+# Finally, we print the output:
+print(f"Minimum cost: {IPmod.objVal}")
+print(f"Time taken: {it2-it1}")
+
+# Let's figure out which local/regional FCs are open
+print(f"Local FCs open: {[v for v in nodes if x[v].x == 1]}")
+print(f"Regional FCs open: {[u for u in nodes if y[u].x == 1]}")
+
+# Lastly, let's figure out the assignments:
+print("Demand location to Local FC assignments: ")
+for i in nodes:
+    for v in nodes:
+        if w[v, i].x == 1:
+            print(f" Demand location {i} --> Local FC {v}")
+print("Local FC to Regional FC assignments: ")
+for v in nodes:
+    for u in nodes:
+        if z[u, v].x == 1:
+            print(f" Local FC {v} --> Regional FC {u}")
+
+
+
+
+
+
 
 
